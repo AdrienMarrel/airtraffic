513438bf7851b54aedc6d2c0c696416eb7b27a51
AdrienMarrel <127510195+AdrienMarrel@users.noreply.github.com>
2023-06-02 13:48:29 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 HOME_AIRPORTS = ('LGW', 'LIS', 'LYS')
 PAIRED_AIRPORTS = ('FUE', 'AMS', 'ORY')
 
-# df = pd.read_parquet("traffic_10lines.parquet")
+traffic_df = pd.read_parquet("c:/Users/adrie/airtraffic/traffic_10lines.parquet")
 
 st.title('Traffic Forecaster')
 
@@ -26,3 +26,109 @@ st.write('Paired Airport selected:', paired_airport)
 st.write('Days of forecast:', nb_days)
 st.write('Date selected:', forecast_date)
 
+# Question 1 : 
+
+def generate_route_df(traffic_df: pd.DataFrame, homeAirport: str, pairedAirport: str) -> pd.DataFrame:
+  """Extract route dataframe from traffic dataframe for route from home airport to paired airport
+
+  Args:
+  - traffic_df (pd.DataFrame): traffic dataframe
+  - homeAirport (str): IATA Code for home airport
+  - pairedAirport (str): IATA Code for paired airport
+
+  Returns:
+  - pd.DataFrame: aggregated daily PAX traffic on route (home-paired)
+  """
+  _df = (traffic_df
+         .query('home_airport == "{home}" and paired_airport == "{paired}"'.format(home=homeAirport, paired=pairedAirport))
+         .groupby(['home_airport', 'paired_airport', 'date'])
+         .agg(pax_total=('pax', 'sum'))
+         .reset_index()
+         )
+  return _df
+
+traffic_liaison_df = generate_route_df(traffic_df, home_airport, paired_airport)
+
+import datetime
+import plotly
+import pandas as pd
+
+import plotly.offline as pyoff
+import plotly.graph_objs as go
+
+from plotly.subplots import make_subplots
+
+def draw_ts_multiple(df: pd.DataFrame, v1: str, v2: str=None, prediction: str=None, date: str='date',
+              secondary_y=True, covid_zone=False, display=True):
+  """Draw times series possibly on two y axis, with COVID period option.
+​
+  Args:
+  - df (pd.DataFrame): time series dataframe (one line per date, series in columns)
+  - v1 (str | list[str]): name or list of names of the series to plot on the first x axis
+  - v2 (str): name of the serie to plot on the second y axis (default: None)
+  - prediction (str): name of v1 hat (prediction) displayed with a dotted line (default: None)
+  - date (str): name of date column for time (default: 'date')
+  - secondary_y (bool): use a secondary y axis if v2 is used (default: True)
+  - covid_zone (bool): highlight COVID-19 period with a grayed rectangle (default: False)
+  - display (bool): display figure otherwise just return the figure (default: True)
+​
+  Returns:
+  - fig (plotly.graph_objs._figure.Figure): Plotly figure generated
+​
+  Notes:
+  Make sure to use the semi-colon trick if you don't want to have the figure displayed twice.
+  Or use `display=False`.
+  """
+  if isinstance(v1, str):
+    variables = [(v1, 'V1')]
+  else:
+    variables = [(v, 'V1.{}'.format(i)) for i, v in enumerate(v1)]
+  title = ''.join([n + ': '+ v for v, n in variables]) + ('V2: ' + v2) if v2 else ''.join([v + ': '+ n for v, n in variables])
+  layout = dict(
+    title=title,
+    xaxis=dict(
+        rangeselector=dict(
+            buttons=list([
+                dict(count=1,
+                     label='1m',
+                     step='month',
+                     stepmode='backward'),
+                dict(count=6,
+                     label='6m',
+                     step='month',
+                     stepmode='backward'),
+                dict(step='all')
+            ])
+        ),
+        rangeslider=dict(
+            visible = True
+        ),
+        type='date'
+    )
+  )
+  fig = make_subplots(specs=[[{"secondary_y": True}]])
+  fig.update_layout(layout)
+  for v, name in variables:
+    fig.add_trace(go.Scatter(x=df[date], y=df[v], name=name), secondary_y=False)
+  if v2:
+    fig.add_trace(go.Scatter(x=df[date], y=df[v2], name='V2'), secondary_y=secondary_y)
+    fig['layout']['yaxis2']['showgrid'] = False
+    fig.update_yaxes(rangemode='tozero')
+    fig.update_layout(margin=dict(t=125 + 30 * (len(variables) - 1)))
+  if prediction:
+    fig.add_trace(go.Scatter(x=df[date], y=df[prediction], name='^V1', line={'dash': 'dot'}), secondary_y=False)
+
+  if covid_zone:
+    fig.add_vrect(
+        x0=pd.Timestamp("2020-03-01"), x1=pd.Timestamp("2022-01-01"),
+        fillcolor="Gray", opacity=0.5,
+        layer="below", line_width=0,
+    )
+  if display:
+    pyoff.iplot(fig)
+  return fig
+
+draw_ts_multiple(
+    traffic_liaison_df,
+    'pax_total' 
+)
